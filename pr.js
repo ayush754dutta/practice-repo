aa3b179da19631e46b5cddc3e04431bcd2244554
Ayush Dutta <ayush.dutta489@gmail.com>
2023-10-20 23:50:54 +0530

    first commit

---
+++ pr.js
@@ -0,0 +1,32 @@
+/** @format */
+
+"use strict";
+
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  if (request.url === "/home") {
+    response.write(`<h1>Welcome to the home page of TechMech.com</h1>`);
+    response.end();
+    return;
+  }
+  if (request.url === "/about") {
+    response.write("<h1>This is our short history</h1>");
+    response.end();
+    return;
+  }
+  response.end(`<h1>OOPS! Page can't be found!</h1>
+  <a href="/home">Go back to home</a>`);
+});
+
+server.listen(5500);
+
+const _ = require("lodash");
+
+const arr = [1, [2, [3, [4]]]];
+
+const arrNew = _.flattenDeep(arr);
+console.log(arrNew);
+
+console.log("hello world");
+console.log("hello people");
